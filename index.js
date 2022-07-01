0035f83ea62e2c2641b9d3f2f81d682f16cb9241
sokret sathit <sokretsathit13@gmail.com>
2022-06-30 19:54:48 -0500

    new changes

--- index.js
+++ index.js
@@ -17,15 +17,38 @@
 //console.log(document.querySelector("#header").className)
 
 
-var newLi = document.createElement("Li")
-newLi.textContent = "3"
 
-var myList = document.getElementById("my-list")
-myList.prepend(newLi)
+//var myList = document.getElementById("my-list")
+//myList.prepend(newLi)
 
-var newP = document.createElement("p")
-newP.textContent = "Baja Prerunner"
-newP.style.fontSize = "50px"
-newP.style.textAlign = "center"
-newP.style.textA = "red"
-document.body.append(newP)
+//var newP = document.createElement("p")
+//newP.textContent = "Baja Prerunner"
+//newP.style.fontSize = "50px"
+//newP.style.textAlign = "center"
+//newP.style.textA = "red"
+//document.body.append(newP)
+
+//var myList = document.getElementById("my-list")
+//myList.innerHTML += "<li>3</li>"
+
+//document.body.innerHTML +=  "<p> id='paragraph' BajAAAAAAAA </p>"
+//document.getElementById("para").style.textAlign = "center"
+
+var powerRangers = [
+    {name:"Jason Lee Scott", color: "Red"},
+    {name:"Kimberly Hart", color: "Pink"} ,
+    {name:"Zack Taylor", color: "Black"},
+    {name:"Trini Kwan", color: "Yellow"},
+    {name:"Billy Cranston", color: "Blue"},
+    {name:"Tommy Oliver", color: "Green"}
+]
+
+var rangersList = document.getElementById("rangers")
+
+for (var i = 0; i < powerRangers.length; i++) {
+    //var newName = document.createElement("li")
+    //newName.textContent = powerRangers[i]
+    //rangersList.append(newName)
+
+    rangersList.innerHTML += "<li>" + powerRangers[i] + "</i>"
+}
